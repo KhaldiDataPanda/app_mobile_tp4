1d37047f04767bcabeed3de4472175c9150750f8
Abdoudevweb <abderrhmane.khaldi@etu.univ-blida.dz>
2023-03-21 02:15:00 +0100

    man ia hate software engineering

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -26,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String[]> mData;
     private CustomAdapter mAdapter;
 
+    private ArrayList<ClipData.Item> listsHolder = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         mButtonAdd = findViewById(R.id.buttonAdd);
         mListView = findViewById(R.id.listView);
 
+        listsHolder= new ArrayList<>();
         mData = new ArrayList<>();
         mAdapter = new CustomAdapter(this, R.layout.list_item, mData);
         mListView.setAdapter(mAdapter);
@@ -55,4 +63,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //on create option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.options_list, menu);
+        return true;
+    }
+
+    //on click option menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.create_new_list) {
+            listsHolder = getItemList();
+            // print list holder
+            for (int i = 0; i < listsHolder.size(); i++) {
+                Log.d("listHolder", (String) listsHolder.get(i).getText());
+            }
+            listsHolder = new ArrayList<>();
+            Toast.makeText(this, "New list created", Toast.LENGTH_SHORT).show();
+            String[] item1 = new String[]{"New Item", "New Item"};
+            //mData.clear();
+            //mData.add(item1);
+            mAdapter.notifyDataSetChanged();
+        }
+        else if (item.getItemId() == R.id.clear_list_item){
+            mData.clear();
+            mAdapter.notifyDataSetChanged();
+        }
+        else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("About");
+            builder.setMessage("This is a simple list app");
+            builder.setPositiveButton("OK", null);
+            builder.show();
+
+        }
+        return true;
+    }
+
+    private ArrayList<ClipData.Item> getItemList() {
+        ArrayList<ClipData.Item> listsHolder = new ArrayList<>();
+        for (int i = 0; i < mListView.getCount(); i++) {
+            View view = mListView.getChildAt(i);
+            EditText nameEditText = view.findViewById(R.id.textView1);
+            EditText quantityEditText = view.findViewById(R.id.textView2);
+            String name = nameEditText.getText().toString();
+            int quantity = Integer.parseInt(quantityEditText.getText().toString());
+            ClipData.Item item = new ClipData.Item(name, String.valueOf(quantity));
+            listsHolder.add(item);
+
+        }
+        return listsHolder;
+    }
+
 }
